97b110bfe895cbe2b003583436da03de3e609cca
Adam Zaninovich <adam.zaninovich@gmail.com>
2022-06-14 12:30:04 -0700

    fix tabs in projects

--- config.el
+++ config.el
@@ -243,6 +243,7 @@
  doom-themes-treemacs-theme "doom-colors")
 
 (after! centaur-tabs
+  (centaur-tabs-group-by-projectile-project)
   (setq
    centaur-tabs-style "bar"
    centaur-tabs-set-bar 'none
@@ -251,31 +252,31 @@
 
   (centaur-tabs-change-fonts (plist-get me/variable-pitch-font :family) 150)
 
-  (defun centaur-tabs-buffer-groups ()
-    "`centaur-tabs-buffer-groups' control buffers' group rules.
+  ;; (defun centaur-tabs-buffer-groups ()
+  ;;   "`centaur-tabs-buffer-groups' control buffers' group rules.
 
-    Group centaur-tabs with mode if buffer is derived from `eshell-mode'
-    `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
-    All buffer name start with * will group to \"Emacs\".
-    Other buffer group by `centaur-tabs-get-group-name' with project name."
-    (list
-     (cond
-      ((or (string-equal "*" (substring (buffer-name) 0 1))
-           (memq major-mode '(magit-process-mode
-                              magit-status-mode
-                              magit-diff-mode
-                              magit-log-mode
-                              magit-file-mode
-                              magit-blob-mode
-                              magit-blame-mode
-                              )))
-       "Emacs")
-      ((derived-mode-p 'eshell-mode) "EShell")
-      ((derived-mode-p 'dired-mode) "Dired")
-      ;; ((derived-mode-p 'emacs-lisp-mode) "Elisp")
-      ;; ((memq major-mode '(org-mode org-agenda-mode diary-mode)) "OrgMode")
-      (t
-       (centaur-tabs-get-group-name (current-buffer))))))
+  ;;   Group centaur-tabs with mode if buffer is derived from `eshell-mode'
+  ;;   `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
+  ;;   All buffer name start with * will group to \"Emacs\".
+  ;;   Other buffer group by `centaur-tabs-get-group-name' with project name."
+  ;;   (list
+  ;;    (cond
+  ;;     ((or (string-equal "*" (substring (buffer-name) 0 1))
+  ;;          (memq major-mode '(magit-process-mode
+  ;;                             magit-status-mode
+  ;;                             magit-diff-mode
+  ;;                             magit-log-mode
+  ;;                             magit-file-mode
+  ;;                             magit-blob-mode
+  ;;                             magit-blame-mode
+  ;;                             )))
+  ;;      "Emacs")
+  ;;     ((derived-mode-p 'eshell-mode) "EShell")
+  ;;     ((derived-mode-p 'dired-mode) "Dired")
+  ;;     ;; ((derived-mode-p 'emacs-lisp-mode) "Elisp")
+  ;;     ;; ((memq major-mode '(org-mode org-agenda-mode diary-mode)) "OrgMode")
+  ;;     (t
+  ;;      (centaur-tabs-get-group-name (current-buffer))))))
   )
 
 (after! projectile
